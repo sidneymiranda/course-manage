ae718687e2d53dc8d8e49642ceab78724e0ced35
Sidney Miranda <sidneysmiranda@hotmail.com>
2020-07-08 17:52:54 -0300

    Adicionando o meu projeto com Angular 8

---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl: './course-list.component.html'
+})
+export class CourseListComponent implements OnInit{
+
+    courses: Course[] = [];
+
+    ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                name: 'Angular Forms',
+                imageUrl: '',
+                price: 500.00,
+                code: 'ATX-500',
+                duration: 120,
+                rating: 4.8,
+                releaseDate: 'March, 3, 2020'
+            },
+
+            {
+                id: 2,
+                name: 'Angular HTTP',
+                imageUrl: '',
+                price: 350.00,
+                code: 'XPTO-237',
+                duration: 80,
+                rating: 4.4,
+                releaseDate: 'April, 15, 2020'
+            }
+
+        ] 
+    }
+
+
+
+
+}
