3ff2a9dce9c3e57c00e3b927ff112b71b2782108
Sidney Miranda <sidneysmiranda@hotmail.com>
2021-03-08 15:12:18 -0300

    Versão do angular atualizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/star.component';
+import { ReplacePipe } from './pipe/replace.pipe';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent, CourseListComponent, StarComponent],
+  declarations: [AppComponent, CourseListComponent, StarComponent, ReplacePipe],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
